6659736caead07daf95415bf71317c7b9865b6ac
auffi <yscoo@hotmail.com>
2025-07-04 17:00:08 +0100

    mars-rover parser updates

--- src/instructions_parser.py
+++ src/instructions_parser.py
@@ -1,11 +1,26 @@
-from src.foundations import Instructions
+from src.foundations import Instructions, Compass, Position, PlateauSize
 from typing import List
 
 class InstructionsParser():
     def parse(instructions_str: str) -> List[Instructions]:
         
+        if not isinstance(instructions_str, str):
+            return []
+        
         parsed = []
         for char in instructions_str:
             if char in [i.value for i in Instructions]:
                 parsed.append(Instructions(char))
+        return parsed
+    
+class CompassParser():
+    def parse(compass_str: str) -> List[Compass]:
+        
+        if not isinstance(compass_str, str):
+            return []
+        
+        parsed = []
+        for char in compass_str:
+            if char in [i.value for i in Compass]:
+                parsed.append(Compass(char))
         return parsed
--- test/test_instructions.py
+++ test/test_instructions.py
@@ -1,9 +1,37 @@
-from src.instructions_parser import InstructionsParser
-from src.foundations import Instructions
+from src.instructions_parser import InstructionsParser, CompassParser
+from src.foundations import Instructions, Compass
 
-def test_parser_returns_list():
+def test_instructions_parser_returns_list():
     input = "L"
-
     parser = InstructionsParser.parse(input)
-
     assert parser == [Instructions.LEFT]
+
+def test_instructions_parser_returns_filtered_list_mixed_inputs():
+    input = "LMPOR"
+    parser = InstructionsParser.parse(input)
+    print("output:", parser)
+    assert parser == [Instructions.LEFT,
+                      Instructions.MOVE,
+                      Instructions.RIGHT]
+    
+def test_instructions_parser_returns_empty_list_if_invalid_inputs():
+    input = "QPAZX"
+    parser = InstructionsParser.parse(input)
+    assert parser == []
+
+def test_compass_parser_returns_list():
+    input = "N"
+    parser = CompassParser.parse(input)
+    assert parser == [Compass.NORTH]
+
+def test_compass_parser_returns_filtered_list_of_mixed_inputs():
+    input = "NP47SEZ"
+    parser = CompassParser.parse(input)
+    assert parser == [Compass.NORTH,
+                      Compass.SOUTH,
+                      Compass.EAST]
+def test_compass_parser_returns_empty_list_if_invalid_inputs():
+    input = 846637
+    parser = CompassParser.parse(input)
+    assert parser == []
+
